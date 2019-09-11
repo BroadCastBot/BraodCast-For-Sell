a02729a67f3006d0376dcaed7f6d2b39a3ca36cc
BroadCastBot <50641382+BroadCastBot@users.noreply.github.com>
2019-09-11 20:45:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`BroadCasting`,"http://twitch.tv/S-F")
+client.user.setGame(`Rúlez.`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 
